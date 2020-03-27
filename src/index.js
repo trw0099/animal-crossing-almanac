efa97e6ba640618d199d454f01edf2f82f182f18
Tyler Wood <tyler@epay3.com>
2020-03-26 19:59:13 -0500

    Fix filter/sort and add font/bg image

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 import ToggleButton from 'react-bootstrap/ToggleButton';
-import Button from 'react-bootstrap/Button';
 import Animals from './data/NorthernAnimals.json';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFish, faBug } from '@fortawesome/free-solid-svg-icons'
@@ -33,23 +32,10 @@ function Animal(props) {
 }
 
 class AnimalList extends React.Component {
-    constructor(props) {
-        super(props);
-
-        let sortedAnimals = [].concat(Animals)
-            .sort((a, b) => {
-                return b.Price - a.Price;
-            });
-
-        this.state = {
-            animalList: sortedAnimals
-        };
-    }
-
     renderAnimal(i) {
         return <Animal
             key={i}
-            value={this.state.animalList[i]}
+            value={this.props.animalList[i]}
         />;
     }
 
@@ -61,11 +47,11 @@ class AnimalList extends React.Component {
             validTime = true;
         }
         else {
-            animal.Times.map((time, i) => {
-                if (time.Start < time.End && (time.Start <= this.props.hour && time.End >= this.props.hour)) {
+            animal.Times.forEach((time) => {
+                if (time.Start < time.End && (time.Start <= this.props.hour && time.End > this.props.hour)) {
                     validTime = true;
                 }
-                else if (time.Start > time.End && (time.Start <= this.props.hour || time.End >= this.props.hour)) {
+                else if (time.Start > time.End && (time.Start <= this.props.hour || time.End > this.props.hour)) {
                     validTime = true;
                 }
             });
@@ -79,7 +65,7 @@ class AnimalList extends React.Component {
             return true;
         }
         else {
-            animal.Seasons.map((season, i) => {
+            animal.Seasons.forEach((season) => {
                 if (season.Start < season.End && (season.Start <= this.props.month && season.End >= this.props.month)) {
                     validMonth = true;
                 }
@@ -92,59 +78,47 @@ class AnimalList extends React.Component {
         return validMonth;
     }
 
-    sort(sortType) {
-        let sortedAnimals = [].concat(Animals)
-            .sort((a, b) => {
-                if (sortType === "price") {
-                    return b.Price - a.Price;
-                }
-                else if (sortType === "location") {
-                    return a.Location - b.Location;
-                }
-                else {
-                    return a.Name - b.Name;
-                }
-            });
-
-        this.setState({
-            animalList: sortedAnimals
-        })
-    }
-
     render() {
         return (
-            <div>
-                <Row>
-                    {
-                        this.state.animalList
-                            .map((fish, i) => {
-                                return this.checkTime(fish) ? this.renderAnimal(i) : '';
-                            })
-                    }
-                </Row>
-            </div>
+            <Row>
+                {
+                    this.props.animalList
+                        .map((fish, i) => {
+                            return this.checkTime(fish) ? this.renderAnimal(i) : '';
+                        })
+                }
+            </Row>
         );
     }
 }
 
-class Game extends React.Component {
+class App extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
             time: new Date().toLocaleString([], { dateStyle: 'long', timeStyle: 'short' }),
             month: new Date().getMonth() + 1,
-            hour: new Date().getHours()
+            hour: new Date().getHours(),
+            animalList: Animals,
+            filter: "all",
+            sort: "price"
         };
     }
+
     componentDidMount() {
         this.intervalID = setInterval(
             () => this.tick(),
             60000
         );
+
+        this.updateList(this.state.filter, this.state.sort);
     }
+
     componentWillUnmount() {
         clearInterval(this.intervalID);
     }
+
     tick() {
         this.setState({
             time: new Date().toLocaleString([], { dateStyle: 'long', timeStyle: 'short' }),
@@ -152,6 +126,62 @@ class Game extends React.Component {
             hour: new Date().getHours()
         });
     }
+
+    updateList(filter, sort) {
+        let filteredAnimals = [].concat(Animals)
+            .filter((animal) => {
+                return filter === "all" || filter === animal.Type;
+            });
+
+        let sortedAnimals = [].concat(filteredAnimals)
+            .sort((a, b) => {
+                if (sort === "price") {
+                    return b.Price - a.Price;
+                }
+                else if (sort === "location") {
+                    if (a.Location < b.Location) {
+                        return -1;
+                    }
+                    if (a.Location > b.Location) {
+                        return 1;
+                    }
+                    return 0;
+                }
+                else {
+                    if (a.Name < b.Name) {
+                        return -1;
+                    }
+                    if (a.Name > b.Name) {
+                        return 1;
+                    }
+                    return 0;
+                }
+            });
+
+        this.setState({
+            animalList: sortedAnimals,
+            filter: filter,
+            sort: sort
+        });
+    }
+
+    filter(filterType) {
+        if (filterType !== "fish" && filterType !== "insect" && filterType !== "all") {
+            alert("Invalid filter");
+        }
+
+        this.updateList(filterType, this.state.sort)
+    }
+
+    sort(sortType) {
+        if (sortType !== "price" && sortType !== "location" && sortType !== "name") {
+            alert("Invalid sort");
+        }
+
+        this.updateList(this.state.filter, sortType)
+    }
+
+
     render() {
         return (
             <Container>
@@ -160,17 +190,25 @@ class Game extends React.Component {
                         <h1>Animal Crossing Almanac</h1>
                     </Col>
                     <Col md={6}>
-                        <p className="float-right">
-                            Current Time: <b>{this.state.time}</b>
+                        <p className="float-right time">
+                            <b>{this.state.time}</b>
                         </p>
                     </Col>
                 </Row>
                 <Row>
-                    <Col className="mb-2">
+                    <Col md="auto" className="mb-3">
                         <ToggleButtonGroup type="radio" name="options" defaultValue={'all'}>
-                            <ToggleButton value={'all'} variant="secondary">All</ToggleButton>
-                            <ToggleButton value={'fish'} variant="primary"><FontAwesomeIcon icon={faFish} /></ToggleButton>
-                            <ToggleButton value={'insects'} variant="success"><FontAwesomeIcon icon={faBug} /></ToggleButton>
+                            <ToggleButton onClick={() => { this.filter("all") }} value={'all'} variant="secondary">All</ToggleButton>
+                            <ToggleButton onClick={() => { this.filter("fish") }} value={'fish'} variant="primary"><FontAwesomeIcon icon={faFish} /></ToggleButton>
+                            <ToggleButton onClick={() => { this.filter("insect") }} value={'insects'} variant="success"><FontAwesomeIcon icon={faBug} /></ToggleButton>
+                        </ToggleButtonGroup>
+
+                    </Col>
+                    <Col className="mb-3">
+                        <ToggleButtonGroup type="radio" name="options" defaultValue={'price'}>
+                            <ToggleButton onClick={() => { this.sort("price") }} value={'price'} variant="secondary">Price</ToggleButton>
+                            <ToggleButton onClick={() => { this.sort("name") }} value={'name'} variant="secondary">Name</ToggleButton>
+                            <ToggleButton onClick={() => { this.sort("location") }} value={'location'} variant="secondary">Location</ToggleButton>
                         </ToggleButtonGroup>
                     </Col>
                 </Row>
@@ -179,7 +217,7 @@ class Game extends React.Component {
 
                     </Col>
                 </Row>
-                <AnimalList month={this.state.month} hour={this.state.hour} />
+                <AnimalList month={this.state.month} hour={this.state.hour} animalList={this.state.animalList} />
             </Container>
         );
     }
@@ -188,6 +226,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-    <Game />,
+    <App />,
     document.getElementById('root')
 );
